c51d09d18a52a53ea84b42a982c046e2aea79ed2
Ryan Tian <140691692+runyangtian@users.noreply.github.com>
2025-06-12 05:27:32 -0700

    Add files via upload

--- src/instmem.v
+++ src/instmem.v
@@ -5,18 +5,18 @@ module instmem(
 );
 
     reg [8:0] memory [0:255];
-    integer i;
-
-    initial begin
-        $readmemb("inst.txt", memory);
-        for (i = 0; i < 256; i = i + 1) begin
-            $display(">>> instmem[%d] = %b", i, memory[i]);
-            if (memory[i] === 9'bx) begin
-                $display("*** instmem[%d] is uninitialized ***", i);
-            end
-        end
-        
-    end
+    
+    // integer i;
+    // initial begin
+    //     $readmemb("inst.txt", memory);
+    //     // $readmemb("../assembler/inst.txt", memory);
+    //     for (i = 0; i < 256; i = i + 1) begin
+    //         $display(">>> instmem[%d] = %b", i, memory[i]);
+    //         if (memory[i] === 9'bx) begin
+    //             $display("*** instmem[%d] is uninitialized ***", i);
+    //         end
+    //     end 
+    // end
 
     always @(posedge clk) begin
         inst <= memory[pc];
