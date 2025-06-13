5e712798785286464a154b65810a6c5eec112688
sbulatao <114209345+sbulatao@users.noreply.github.com>
2025-06-12 18:19:54 -0700

    Update instmem.v

--- src/instmem.v
+++ src/instmem.v
@@ -7,8 +7,8 @@ module instmem(
     reg [8:0] memory [0:255];
     
     // integer i;
-    // initial begin
-    //     $readmemb("inst.txt", memory);
+    initial begin
+        $readmemb("inst.txt", memory); // NEED THIS to read inst.txt
     //     // $readmemb("../assembler/inst.txt", memory);
     //     for (i = 0; i < 256; i = i + 1) begin
     //         $display(">>> instmem[%d] = %b", i, memory[i]);
@@ -16,7 +16,7 @@ module instmem(
     //             $display("*** instmem[%d] is uninitialized ***", i);
     //         end
     //     end 
-    // end
+    end
 
     always @(posedge clk) begin
         inst <= memory[pc];
